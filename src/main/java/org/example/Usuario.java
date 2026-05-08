f78197aea855e4028147b48c9bc73de123030b22
celes <celeozer@hotmail.com>
2026-05-08 11:29:34 -0300

    first commit

---
+++ src/main/java/org/example/Usuario.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Usuario {
+
+    private String name;
+    private String email;
+    private String password;
+
+    public Usuario(String name, String email, String password){
+        this.password = password;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
